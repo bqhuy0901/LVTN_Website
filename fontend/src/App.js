d872962867e4eb1f2b1d86770fed804101a58456
bqhuy0901 <bqhuy0901@gmail.com>
2022-10-27 13:09:03 +0700

    User Login Dang hoan thanh

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Webfont from "webfontloader";
 import Header from "./component/layout/Header/Header";
 import Footer from "./component/layout/Footer/Footer";
 import { useEffect } from "react";
+import LoginSignUp from "./component/User/Login/LoginSignUp";
 
 function App() {
   useEffect(() => {
     Webfont.load({
       families: ["Roboto", "Droid Sans", "Chilanka"],
     });
-  });
+  }, []);
 
   return (
     <Router>
       <Header />
+      <Route exact path="/login" component={LoginSignUp} />
       <Footer />
     </Router>
   );
