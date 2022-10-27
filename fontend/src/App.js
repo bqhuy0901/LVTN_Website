11d05d77371abf6a7892aacca2b49a9b8a407a37
bqhuy0901 <bqhuy0901@gmail.com>
2022-10-27 12:37:48 +0700

    add app

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Webfont from "webfontloader";
 import Header from "./component/layout/Header/Header";
 import Footer from "./component/layout/Footer/Footer";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    Webfont.load({
+      families: ["Roboto", "Droid Sans", "Chilanka"],
+    });
+  });
+
   return (
     <Router>
       <Header />
