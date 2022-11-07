214c1706f3370cb118e2b3941b88ab2365cdb171
bqhuy0901 <71885974+bqhuy0901@users.noreply.github.com>
2022-11-07 20:30:15 +0700

    Update paymentController.js

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -11,12 +11,12 @@ exports.processPayment = catchAsyncError(async (req, res, next) => {
     },
   });
 
-  res.status.json({
+  res.status(200).json({
     success: true,
     client_secret: myPayment.client_secret,
   });
 });
 
 exports.sendStripeApiKey = catchAsyncError(async (req, res, next) => {
-  res.status.json({ stripeApiKey: process.env.STRIPE_API_KEY });
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
 });
